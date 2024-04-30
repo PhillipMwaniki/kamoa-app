882316c4b8bbfacce3e15429c02c8c0f1c3437e7
Phillip Mwaniki Nzuli <codezeroprivate@gmail.com>
2024-05-01 00:14:05 +0300

    added progress indicator

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {
 import Header from './components/Header';
 import { StyledImage, StyledSafeAreaView } from './components/StyledComponents';
 import Features from './components/Features';
+import ProgressIndicator from './components/ProgressIndicator';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -28,6 +29,7 @@ function App(): React.JSX.Element {
             className="mx-auto mt-12"
           />
           <Features />
+          <ProgressIndicator step={1} length={3} />
         </View>
       </ScrollView>
     </StyledSafeAreaView>
---
+++ src/components/ProgressIndicator.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import { StyledText, StyledView } from './StyledComponents';
+
+type ProgressIndicatorProps = {
+    step: number,
+    length: number
+};
+const ProgressIndicator: React.FunctionComponent<ProgressIndicatorProps> = ({ step, length }) => {
+    return (
+        <StyledView className='mt-6 mx-auto flex flex-col items-center'>
+            <StyledText className='font-barlowCondensedBold leading-4 text-[12px] font-medium text-black mb-2'>Step {step}/{length}</StyledText>
+            <StyledView className='w-[100px] bg-amber-300 rounded-full h-2.5 dark:bg-gray-700'>
+                <StyledView className='bg-green-400 h-2.5 rounded-full' style={{ width: `${(step / length) * 100}%` }} />
+            </StyledView>
+        </StyledView>
+    )
+}
+
+export default ProgressIndicator
