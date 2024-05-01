6524550cc23d059616720696dd6202c76bba62a8
Phillip Mwaniki Nzuli <codezeroprivate@gmail.com>
2024-05-01 10:51:24 +0300

    added absolute path file loading

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['nativewind/babel'],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@images': './src/images',
+        },
+      },
+    ],
+  ],
 };
